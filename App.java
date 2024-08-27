6315609f6d798ff4a3b744a9f05c766be82d4413
rodrigolb01 <rodrigolb01@gmail.com>
2024-08-26 23:35:15 -0300

    binary search

--- App.java
+++ App.java
@@ -1,8 +1,47 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class App {
 
+  static int binarySearch(int target, int[] arr) {
+    int start = 0;
+    int end = arr.length - 1;
+    int mid = 0;
+    boolean found = false;
+
+    while (start <= end) {
+      mid = (int) ((end + start) / 2);
+      if (arr[mid] == target) {
+        found = true;
+        break;
+      } else if (arr[mid] < target) {
+        start = mid + 1;
+      } else if (arr[mid] > target) {
+        end = mid - 1;
+      }
+    }
+    if (found == true) {
+      return mid;
+    } else {
+      return -1;
+    }
+  }
+
   public static void main(String[] args) {
+    int[] list = new int[10];
+    for (int i = 0; i < 10; i++) {
+      list[i] = i * 2;
+    }
+
+    for (int i = 0; i < list.length; i++) {
+      System.out.println(list[i]);
+    }
+
+    Scanner sc = new Scanner(System.in);
+    System.out.println("find number: ");
+    int target = sc.nextInt();
+    int res = binarySearch(target, list);
+    System.out.println("number " + target + " found in position " + res);
     // int[] arr1 = {1,2,3,4,5};
     // NormalList l = new NormalList(arr1);
     // l.print();
@@ -55,68 +94,68 @@ public class App {
     //   );
     // }
 
-    // NORMAL LINKED LIST//
-    LinkedList<Integer> list2 = new LinkedList<Integer>();
+    // // NORMAL LINKED LIST//
+    // LinkedList<Integer> list2 = new LinkedList<Integer>();
 
-    for (int i = 0; i < 10000; i++) {
-      list2.add(i);
-    }
+    // for (int i = 0; i < 10000; i++) {
+    //   list2.add(i);
+    // }
 
-    long startTime = System.currentTimeMillis();
-    for (int i = 0; i < list2.getSize(); i++) {
-      // System.out.println(
-      //   "value in position " + i + " : " + list2.get(i).getValue()
-      // );
-      list2.get(i);
-    }
-    long endTime = System.currentTimeMillis();
-    long elapsedTime = endTime - startTime;
-    System.out.println(
-      "The elapsed time for acessing linked list was: " +
-      elapsedTime +
-      " miliseconds."
-    );
+    // long startTime = System.currentTimeMillis();
+    // for (int i = 0; i < list2.getSize(); i++) {
+    //   // System.out.println(
+    //   //   "value in position " + i + " : " + list2.get(i).getValue()
+    //   // );
+    //   list2.get(i);
+    // }
+    // long endTime = System.currentTimeMillis();
+    // long elapsedTime = endTime - startTime;
+    // System.out.println(
+    //   "The elapsed time for acessing linked list was: " +
+    //   elapsedTime +
+    //   " miliseconds."
+    // );
 
-    // LINKED LIST WITH ITERATOR //
+    // // LINKED LIST WITH ITERATOR //
 
-    LinkedList<Integer> l2Iterate = new LinkedList<Integer>();
+    // LinkedList<Integer> l2Iterate = new LinkedList<Integer>();
 
-    startTime = System.currentTimeMillis();
-    for (int i = 0; i < 10000; i++) {
-      l2Iterate.add(i);
-    }
+    // startTime = System.currentTimeMillis();
+    // for (int i = 0; i < 10000; i++) {
+    //   l2Iterate.add(i);
+    // }
 
-    LinkedListIterator<Integer> iterator = l2Iterate.getIterator();
+    // LinkedListIterator<Integer> iterator = l2Iterate.getIterator();
 
-    while (iterator.nextexists()) {
-      iterator.getNext();
-    }
-    endTime = System.currentTimeMillis();
-    elapsedTime = endTime - startTime;
-    System.out.println(
-      "The elapsed time for acessing linked list with interation was: " +
-      elapsedTime +
-      " miliseconds."
-    );
+    // while (iterator.nextexists()) {
+    //   iterator.getNext();
+    // }
+    // endTime = System.currentTimeMillis();
+    // elapsedTime = endTime - startTime;
+    // System.out.println(
+    //   "The elapsed time for acessing linked list with interation was: " +
+    //   elapsedTime +
+    //   " miliseconds."
+    // );
 
-    // ARRAY LIST//
+    // // ARRAY LIST//
 
-    startTime = System.currentTimeMillis();
-    ArrayList<Integer> al = new ArrayList<Integer>();
-    for (int i = 0; i < 10000; i++) {
-      al.add(i);
-    }
+    // startTime = System.currentTimeMillis();
+    // ArrayList<Integer> al = new ArrayList<Integer>();
+    // for (int i = 0; i < 10000; i++) {
+    //   al.add(i);
+    // }
 
-    for (int i = 0; i < al.size(); i++) {
-      al.get(i);
-    }
-    endTime = System.currentTimeMillis();
-    elapsedTime = endTime - startTime;
-    System.out.println(
-      "The elapsed time for acessing arraylist was: " +
-      elapsedTime +
-      " miliseconds."
-    );
+    // for (int i = 0; i < al.size(); i++) {
+    //   al.get(i);
+    // }
+    // endTime = System.currentTimeMillis();
+    // elapsedTime = endTime - startTime;
+    // System.out.println(
+    //   "The elapsed time for acessing arraylist was: " +
+    //   elapsedTime +
+    //   " miliseconds."
+    // );
     // LinkedList<Employee> list3 = new LinkedList<Employee>();
     // System.out.println("current size: " + list3.getSize());
 
@@ -133,5 +172,6 @@ public class App {
     //     "value in position " + i + " : \n" + list3.get(i).getValue()
     //   );
     // }
+
   }
 }
